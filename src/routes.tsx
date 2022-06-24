85e7a23923e975c61b4ce0bb084753299e01ed10
ericpandrade <ericpandrade085@gmail.com>
2022-06-24 18:13:24 -0300

    feat: Initialize Project,  configure react-router-dom and finalize design  home page with responsivity

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
